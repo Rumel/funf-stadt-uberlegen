5b6bb6080cd11aca6a84ee0472d201c200c4fa93
Dalton Dick <daltondick2010@gmail.com>
2019-08-19 00:00:22 -0500

    Change score boxes

--- client/src/components/match.js
+++ client/src/components/match.js
@@ -32,34 +32,38 @@ class Match extends Component {
     const secondEntry = _.find(entries, (e) => e.id === match.league_entry_2);
 
     return(
-      <div className="col-12 col-sm-6">
+      <div className="col-12 col-md-6">
         <div className="match-box">
           <div className="row">
             <div className="col-12">
               <div className="row">
                 <div className="col-9 text-left">
-                  <p>
+                  <p className="match-team-text">
                     <a href={this.getTeamLink(firstEntry.entry_id, selectedWeek)} target="_blank" rel="noopener noreferrer">
                       {firstEntry.entry_name}
                     </a>
                   </p>
                 </div>
                 <div className="col-3">
-                  <p>{this.calculatePoints(firstEntry.entry_id, currentWeek, selectedWeek, match.league_entry_1_points, picks, live)}</p>
+                  <p className="match-score-text">
+                    {this.calculatePoints(firstEntry.entry_id, currentWeek, selectedWeek, match.league_entry_1_points, picks, live)}
+                  </p>
                 </div>
               </div>
             </div>
             <div className="col-12">
               <div className="row">
                 <div className="col-9 text-left">
-                  <p>
+                  <p className="match-team-text">
                     <a href={this.getTeamLink(secondEntry.entry_id, selectedWeek)} target="_blank" rel="noopener noreferrer">
                       {secondEntry.entry_name}
                     </a>
                   </p>
                 </div>
                 <div className="col-3">
-                <p>{this.calculatePoints(secondEntry.entry_id, currentWeek, selectedWeek, match.league_entry_2_points, picks, live)}</p>
+                <p className="match-score-text">
+                  {this.calculatePoints(secondEntry.entry_id, currentWeek, selectedWeek, match.league_entry_2_points, picks, live)}
+                </p>
                 </div>
               </div>
             </div>
